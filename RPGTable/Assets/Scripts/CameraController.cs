8b5da07e2f721a9cb6f5b7b5b157bb5886bbcc8c
Mario <mario_scipio@hotmail.es>
2022-09-20 10:29:30 +0200

    Camera zoom
    Added functionality for the camera to zoom in and out

--- RPGTable/Assets/Scripts/CameraController.cs
+++ RPGTable/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,9 @@ public class CameraController : MonoBehaviour
 
     Vector3 camOffset;
     float rotationSpeed = 1.0f;
+    float minFov = 20f;
+    float maxFov = 70f;
+    float zoomSpeed = 10f;
 
     private void Start()
     {
@@ -18,11 +22,20 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            RotateCamera();
+            OrbitCamera();
         }
+        UpdateZoom();
     }
 
-    void RotateCamera()
+    private void UpdateZoom()
+    {
+        float fov = Camera.main.fieldOfView;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+
+    private void OrbitCamera()
     {
         Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
 
