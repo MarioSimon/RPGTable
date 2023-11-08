99d398c0b7e5bdb2b2597a218135ff6aeed6e39a
Mario <mario_scipio@hotmail.es>
2023-11-08 09:57:25 +0100

    Camera movement boundaries

--- RPGTable/Assets/Scripts/CameraController.cs
+++ RPGTable/Assets/Scripts/CameraController.cs
@@ -43,19 +43,19 @@ public class CameraController : MonoBehaviour
 
     private void HandleMovementInput()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && transform.position.z < 22.5f)
         {
             newPosition += transform.forward * movementSpeed;
         }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && transform.position.x > -22.5f)
         {
             newPosition -= transform.right * movementSpeed;
         }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && transform.position.z > -22.5f)
         {
             newPosition -= transform.forward * movementSpeed;
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && transform.position.x < 22.5f)
         {
             newPosition += transform.right * movementSpeed;
         }
